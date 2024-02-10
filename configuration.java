812307b64f65d3b1ecff386020a767d699b642a4
Abhi-727 <chalamcharlaa3@gmail.com>
2024-02-10 13:22:55 +0530

    New Line Added

--- configuration.java
+++ configuration.java
@@ -26,9 +26,7 @@ public class CsvBatchConfig {
 	@Autowired
 	private CensusRepository censusRepository;
 
-	private int a = 10;
-	
-	private String str = "Hemanth";
+	private String s = "New Line Addition";
 	
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
